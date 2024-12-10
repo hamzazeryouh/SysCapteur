e7d9b98e186f249e63d191ba9e895214b6e59625
hamza zeyouh <zeryouhbusiness@gmail.com>
2024-12-10 20:58:41 +0100

    add model Sensor

---
+++ Sys.Domain/Entities/Sensor/Sensor.cs
@@ -0,0 +1,17 @@
+﻿using Sys.Domain.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sys.Domain.Entities.Sensor
+{
+    public class Sensor : Entity<int>
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Location { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
