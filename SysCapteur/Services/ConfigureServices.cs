72899b7d89ed0ab4b026c2b4ea95d172edfc1723
TEST <TEST@DESKTOP-3MARNQE>
2024-12-10 20:30:30 +0100

    Add project files.

---
+++ SysCapteur/Services/ConfigureServices.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SysCapteur.Services
+{
+    public static class ConfigureServices
+    {
+        public readonly IConfiguration configuration;
+        public ConfigureServices(IConfiguration Configuration)
+        {
+            configuration = Configuration;
+        }
+        public static void Services(IServiceCollection services,)
+        {
+            var jwtSettings = configuration.GetSection("Jwt");
+            var secretKey = jwtSettings["SecretKey"]!;
+            var issuer = jwtSettings["Issuer"]!;
+            var audience = jwtSettings["Audience"]!;
+            var expirationMinutes = int.Parse(jwtSettings["ExpirationMinutes"]!);
+            // Ajouter AuthService comme singleton
+            services.AddSingleton<IAuthService>(new AuthService(secretKey, issuer, audience, expirationMinutes));
+
+            // Configurer l'authentification JWT
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience
+                    };
+                });
+
+            services.AddControllers();
+        }
+    }
+}
