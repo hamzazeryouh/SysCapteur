b3c4ed9f40fb764f6f99e36a6130dff9a681fb19
hamza zeyouh <zeryouhbusiness@gmail.com>
2024-12-10 20:46:11 +0100

    add base entities

---
+++ Sys.Domain/Entities/BaseEntities/Entity.cs
@@ -0,0 +1,39 @@
+﻿using Sys.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sys.Domain.Entities.BaseEntities
+{
+    /// <summary>
+    /// Entity class that implement <see cref="IEntity"/>, which is the base entity class
+    /// </summary>
+    public abstract class Entity : IEntity
+    {
+        /// <summary>
+        /// the creation time of the model
+        /// </summary>
+        public DateTimeOffset CreatedOn { get; set; }
+
+        /// <summary>
+        /// the last time the model has been modified
+        /// </summary>
+        public DateTimeOffset? LastModifiedOn { get; set; }
+
+    }
+
+    /// <summary>
+    /// Entity class that implement <see cref="IEntity"/> and inherit from the <see cref="Entity"/> base class
+    /// </summary>
+    /// <typeparam name="Tkey">type of key</typeparam>
+    public abstract class Entity<Tkey> : Entity, IEntity<Tkey>
+    {
+        /// <summary>
+        /// the id of the entity
+        /// </summary>
+        public Tkey Id { get; set; }
+    }
+}
---
+++ Sys.Domain/Interfaces/IEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sys.Domain.Interfaces
+{
+
+    /// <summary>
+    /// a class that defines an entity
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// the creation time of the model
+        /// </summary>
+        System.DateTimeOffset CreatedOn { get; set; }
+
+        /// <summary>
+        /// the last time the model has been modified
+        /// </summary>
+        System.DateTimeOffset? LastModifiedOn { get; set; }
+    }
+
+    /// <summary>
+    /// a class that defines an entity with a an id
+    /// </summary>
+    /// <typeparam name="Tkey">the type of the id</typeparam>
+    public interface IEntity<Tkey> : IEntity
+    {
+        /// <summary>
+        /// the id of the entity
+        /// </summary>
+        Tkey Id { get; set; }
+    }
+}
+
